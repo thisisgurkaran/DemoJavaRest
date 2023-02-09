91c76081359f95e4e3e47bde2bc1fc1242c01de8
thisisgurkaran <gurkaran.12@gmail.com>
2023-02-09 01:22:16 -0500

    Added some hello world APIs

--- demojavarest/src/main/java/com/karan/rest/service/demojavarest/TestController.java
+++ demojavarest/src/main/java/com/karan/rest/service/demojavarest/TestController.java
@@ -3,13 +3,22 @@ package com.karan.rest.service.demojavarest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import javax.persistance.Entity;
 
 @Controller
 public class TestController {
 
     @RequestMapping("/test")
     @ResponseBody
-    public String handle() {
-        return "this is just for fun";
+    public String test() {
+        // here you can do stuff easy bro
+        return "this is test";
+    }
+
+    @RequestMapping("/test1")
+    @ResponseBody
+    public HelloWorldBean test1() {
+        return new HelloWorldBean("hello world");
+        // return "This is Test1";
     }
 }
